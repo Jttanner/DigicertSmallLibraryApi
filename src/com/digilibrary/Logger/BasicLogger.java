78676363e6ba5fecc6729665f4d880ce484febbe
jonttanner <jonttanner@gmail.com>
2021-09-07 22:35:37 -0600

    Actually make the logger a singleton

--- src/com/digilibrary/Logger/BasicLogger.java
+++ src/com/digilibrary/Logger/BasicLogger.java
@@ -5,6 +5,24 @@ package com.digilibrary.Logger;
 //just prints logging
 public class BasicLogger implements ILogger {
 
+    private static volatile BasicLogger instance;
+
+    public static BasicLogger getInstance(){
+        if (instance == null){
+            //lock if we need to create the singleton instance
+            synchronized (BasicLogger.class){
+                if (instance == null){
+                    instance = new BasicLogger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private BasicLogger(){
+
+    }
+
     @Override
     public void LogInfo(String info) {
         System.out.println(info);
